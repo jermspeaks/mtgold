7fcf942442169023f2ffe0e3189e93290bb2804a
Jeremy Wong <jeremy.wong@clearlabs.com>
2021-08-31 14:50:36 -0700

    Add tailwind to the project. Set up landing page

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    colors: {},
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
